b20ca5ec9946a689900c48ce8adca31773992e85
RidhaFauziyya <ridhafauziyyar123@gmail.com>
2023-03-01 09:52:34 +0700

    Count and Time

--- app/src/main/java/com/example/aplikasiwidget/NewAppWidget.java
+++ app/src/main/java/com/example/aplikasiwidget/NewAppWidget.java
@@ -1,22 +1,55 @@
 package com.example.aplikasiwidget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
 
+    private static final String mSharedPrefFile = "com.example.aplikasiwidget";
+    private static final String COUNT_KEY = "count";
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        SharedPreferences prefs = context.getSharedPreferences(mSharedPrefFile, 0);
+        int count = prefs.getInt(COUNT_KEY+appWidgetId, 0);
+        count++;
+
+        SharedPreferences.Editor prefEditor = prefs.edit();
+        prefEditor.putInt(COUNT_KEY+appWidgetId, count);
+        prefEditor.apply();
+
+        //Append appWidgetID agar membedakan antar widget apabila ada 2 atau lebih widget
+
+        String dateString = DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
+
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
         views.setTextViewText(R.id.appWidget_id, String.valueOf(appWidgetId));
+        views.setTextViewText(R.id.appWidget_update, dateString);
+        views.setTextViewText(R.id.appWidget_count, String.valueOf(count));
+
+        Intent intent = new Intent(context, NewAppWidget.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+
+        int[] idArray = new int[] {appWidgetId};
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, appWidgetId, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        views.setOnClickPendingIntent(R.id.button_update, pendingIntent);
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
